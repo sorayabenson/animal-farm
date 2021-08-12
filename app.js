974580979a13363156f4c844fefbff65f6eb04ca
Soraya Benson <soraya.benson@gmail.com>
2021-08-12 09:49:45 -0700

    the loud boys have captions now

--- app.js
+++ app.js
@@ -5,16 +5,20 @@ const catButton = document.getElementById('catButton');
 const dogSound = document.getElementById('dog-sound');
 const horseSound = document.getElementById('horse-sound');
 const catSound = document.getElementById('cat-sound');
+const animalCaption = document.getElementById('animal')
 
 
 dogButton.addEventListener('click', () => {
   dogSound.play();
+  animalCaption.textContent = '*baaaaaark*';
 });
 
 horseButton.addEventListener('click', () => {
   horseSound.play();
+  animalCaption.textContent = '*ney ney*';
 });
 
 catButton.addEventListener('click', () => {
   catSound.play();
+  animalCaption.textContent = "*where's my knife?*";
 })
